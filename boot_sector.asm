ecbb123827519b48d63371cdf8a3bb4524ba90b1
pokitoz <florian.depraz@epfl.ch>
2015-12-28 14:37:46 +0100

    Helloworld boot loader

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,6 +1,30 @@
-; First boot sector
-loop : 				; Define label to loop forever.
-jmp loop 			; Jumps to label
+; Boot loader print HelloWorld
+
+mov ah, 0x0e
+
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'W'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'D'
+int 0x10
+
+loop:
+jmp loop
 times 510 -( $ - $$ ) db 0 	; Fill the program with 510 bytes (db 0) to the 510 byte.
 dw 0xaa55 			; Magic number for boot sector.
 
