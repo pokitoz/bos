e22ffecdd7bf9e08fdb99af8c4d0810c70fc1e0a
pokitoz <florian.depraz@epfl.ch>
2015-12-28 15:33:57 +0100

    Print Helloworld with stack

--- boot_sector.asm
+++ boot_sector.asm
@@ -2,31 +2,55 @@
 
 mov ah, 0x0e
 
+; Set Stack base
+mov bp, 0x8000
+; Set Stack pointer to base
+mov sp, bp
+
+push 'W'
+push 'l'
+push 'e'
+
+
 mov bx, print_char_H
 add bx, 0x7c00
 mov al, [bx]
 int 0x10
-mov al, 'e'
+
+pop bx
+mov al, bl
 int 0x10
-mov al, 'l'
+
+pop bx
+mov al, bl
 int 0x10
-mov al, 'l'
+
 int 0x10
+
 mov al, 'o'
 int 0x10
-mov al, 'W'
+
+pop bx
+mov al, bl
 int 0x10
+
 mov al, 'o'
 int 0x10
+
 mov al, 'r'
 int 0x10
+
 mov al, 'l'
 int 0x10
-mov al, 'D'
+
+mov al, 'd'
 int 0x10
 
 print_char_H:
 	db "H"
+
+string :
+	db 'Booting OS' , 0 ; Null terminated
 
 loop:
 jmp loop
